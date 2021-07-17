7bb1b7cda83e79aae38826f7c6fe4ea534214520
Saaappi <alex.metz1024@gmail.com>
2021-07-17 02:08:38 -0500

    Added support for getting a creature's ID, name, and location and adding that information into a table.

--- Control/Creature.lua
+++ Control/Creature.lua
@@ -2,15 +2,42 @@ local addonName, addonTable = ...
 local e = CreateFrame("Frame")
 local L = addonTable.L
 
+local unitType, unitID, unitGUID, unitName
+
 e:RegisterEvent("NAME_PLATE_UNIT_ADDED")
 e:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
 
 e:SetScript("OnEvent", function(self, event, ...)
+	-- The reason we specify Vehicle as a supported unit's type is due to some creatures being considered Vehicles by Blizzard. No idea why.
 	if (event == "NAME_PLATE_UNIT_ADDED") then
-	
+		local unitToken = ...
+		if (unitToken) then
+			if (UnitGUID(unitToken) ~= nil) then
+				unitGUID = UnitGUID(unitToken)
+				unitType, _, _, _, _, unitID = strsplit("-", unitGUID); unitID = tonumber(unitID)
+				if (unitType == "Creature" or unitType == "Vehicle") then
+					unitName = UnitName(unitToken)
+					LastSeenCreatures[unitID] = {
+						creatureName = unitName,
+						map = addonTable.map,
+					}
+				end
+			end
+		end
 	end
 	
 	if (event == "UPDATE_MOUSEOVER_UNIT") then
-	
+		local unit = "mouseover"
+		if (UnitGUID(unit) ~= nil) then
+			unitGUID = UnitGUID(unit)
+			unitType, _, _, _, _, unitID = strsplit("-", unitGUID); unitID = tonumber(unitID)
+			if (unitType == "Creature" or unitType == "Vehicle") then
+				unitName = UnitName(unit)
+				LastSeenCreatures[unitID] = {
+					creatureName = unitName,
+					map = addonTable.map,
+				}
+			end
+		end
 	end
 end)
