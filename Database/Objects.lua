77f51dd5382446747c799f21d41d44e8427ffef6
Saaappi <alex.metz1024@gmail.com>
2021-07-19 03:10:00 -0500

    Added the rest of the Tasty Mawshrooms.

--- Database/Objects.lua
+++ Database/Objects.lua
@@ -5,6 +5,10 @@ local objects = {
 	[75293]			= L["Large Battered Chest"],
 	[214383]		= L["Cache of Pure Energy"],
 	[225817]		= L["Spoils of Blackfathom"],
+	[369327]		= L["Invasive Mawshroom"],
+	[369329]		= L["Invasive Mawshroom"],
+	[369330]		= L["Invasive Mawshroom"],
+	[369331]		= L["Invasive Mawshroom"],
 	[369332]		= L["Invasive Mawshroom"],
 }
 
