b5b0aedbec3cf3a388696c1c9463577f6bf1ba5b
Saaappi <alex.metz1024@gmail.com>
2021-07-21 01:41:53 -0500

    Added some comments to the IgnoredItems and SpecialItems files. Not that most people will look--but I'll know they exist.

--- Database/IgnoredItems.lua
+++ Database/IgnoredItems.lua
@@ -1,6 +1,8 @@
 local addonName, addonTable = ...
 local L = addonTable.L
 
+-- These are items that don't need to be tracked because people won't care.
+-- Examples: Anima, Cataloged Research, Azerite, etc.
 local ignoredItems = {
 }
 
--- Database/SpecialItems.lua
+++ Database/SpecialItems.lua
@@ -1,6 +1,8 @@
 local addonName, addonTable = ...
 local L = addonTable.L
 
+-- These are items that people want to treat a bit different than their everyday blues and purps.
+-- Examples: Tasty Mawshrooms, a Mount (love 'em!), etc.
 local specialItems = {
 }
 
