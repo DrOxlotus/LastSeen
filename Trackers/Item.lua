957161b2acb3a2a15159da002f86237bb5e382d7
Saaappi <alex.metz1024@gmail.com>
2021-07-23 01:12:58 -0500

    Removed a debug print. Added a nil check when players loot items like lockboxes or other bag-like items.

--- Trackers/Item.lua
+++ Trackers/Item.lua
@@ -28,95 +28,96 @@ e:SetScript("OnEvent", function(self, event, ...)
 		for i = 1, numLootItems do
 			itemLink = GetLootSlotLink(i)
 			if (itemLink) then
-				lootSourceGUID = (GetLootSourceInfo(i)) print(lootSourceGUID)
+				lootSourceGUID = (GetLootSourceInfo(i))
 				if (lootSourceGUID) then
 					_, _, _, _, _, entityID = strsplit("-", lootSourceGUID); entityID = tonumber(entityID)
-					_, itemID = strsplit(":", itemLink); itemID = tonumber(itemID)
-					itemName, itemLink, itemQuality, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice, itemClassID, itemSubclassID, itemBindType, itemExpacID, itemSetID, itemIsCraftingReagent = GetItemInfo(itemLink)
-					if (itemQuality == nil) then return end
-					-- We need to filter out items that we don't care about.
-					--[[if (itemQuality < 1) then return end
-					for type = 1, #ignoredItemTypes do
-						if (ignoredItemTypes[type] == itemType) then return end
-						if (ignoredItemTypes[type] == itemSubType) then return end
-					end
-					for _, ignoredItemID in ipairs(addonTable.ignoredItems) do
-						if (ignoredItemID == itemID) then return end
-					end
-					for _, ignoredItemID in ipairs(LastSeenIgnoredItems) do
-						if (ignoredItemID == itemID) then return end
-					end]]
-					-- Let's get the current patch and date to store in the table.
-					patch = GetBuildInfo()
-					lootDate = date("%d/%m/%y")
-					-- A creature ID isn't always available, so let's test for that.
-					if (LastSeenCreatures[entityID]) then
-						source = LastSeenCreatures[entityID].creatureName
-						map = LastSeenCreatures[entityID].map
-					elseif (addonTable.objects[entityID]) then
-						-- A valid creature wasn't found in the Creatures table to match with the item. Let's check the Objects table now.
-						source = addonTable.objects[entityID]
-						map = C_Map.GetMapInfo(C_Map.GetBestMapForUnit("player")).name
-					else
-						source = L["Unknown Source"]
-						map = C_Map.GetMapInfo(C_Map.GetBestMapForUnit("player")).name
-						print(L["Colored Addon Name"] .. L["Info Source Unavailable"] .. " @" .. L["Discord"] .. "\n" .. C_Map.GetMapInfo(C_Map.GetBestMapForUnit("player")).name .. ", [" .. itemLink .. ", " .. entityID .. "]")
-					end
-					-- The item shouldn't be filtered out, so let's add it to the Items table.
-					if (LastSeenItems[itemID]) then
-						print(itemID)
-						-- This isn't a NEW item. Confirm if any information requires an update.
-						if (LastSeenItems[itemID]["sourceInfo"].lootDate ~= lootDate) then
-							LastSeenItems[itemID]["sourceInfo"].lootDate = lootDate
+					if (entityID) then -- This protects against nil errors when players loot items like lockboxes.
+						_, itemID = strsplit(":", itemLink); itemID = tonumber(itemID)
+						itemName, itemLink, itemQuality, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice, itemClassID, itemSubclassID, itemBindType, itemExpacID, itemSetID, itemIsCraftingReagent = GetItemInfo(itemLink)
+						if (itemQuality == nil) then return end
+						-- We need to filter out items that we don't care about.
+						--[[if (itemQuality < 1) then return end
+						for type = 1, #ignoredItemTypes do
+							if (ignoredItemTypes[type] == itemType) then return end
+							if (ignoredItemTypes[type] == itemSubType) then return end
 						end
-						if (LastSeenItems[itemID]["sourceInfo"].patch ~= patch) then
-							LastSeenItems[itemID]["sourceInfo"].patch = patch
+						for _, ignoredItemID in ipairs(addonTable.ignoredItems) do
+							if (ignoredItemID == itemID) then return end
 						end
-						if (LastSeenItems[itemID]["sourceInfo"].map ~= addonTable.uiMapName) then
-							LastSeenItems[itemID]["sourceInfo"].map = addonTable.uiMapName
+						for _, ignoredItemID in ipairs(LastSeenIgnoredItems) do
+							if (ignoredItemID == itemID) then return end
+						end]]
+						-- Let's get the current patch and date to store in the table.
+						patch = GetBuildInfo()
+						lootDate = date("%d/%m/%y")
+						-- A creature ID isn't always available, so let's test for that.
+						if (LastSeenCreatures[entityID]) then
+							source = LastSeenCreatures[entityID].creatureName
+							map = LastSeenCreatures[entityID].map
+						elseif (addonTable.objects[entityID]) then
+							-- A valid creature wasn't found in the Creatures table to match with the item. Let's check the Objects table now.
+							source = addonTable.objects[entityID]
+							map = C_Map.GetMapInfo(C_Map.GetBestMapForUnit("player")).name
+						else
+							source = L["Unknown Source"]
+							map = C_Map.GetMapInfo(C_Map.GetBestMapForUnit("player")).name
+							print(L["Colored Addon Name"] .. L["Info Source Unavailable"] .. " @" .. L["Discord"] .. "\n" .. C_Map.GetMapInfo(C_Map.GetBestMapForUnit("player")).name .. ", [" .. itemLink .. ", " .. entityID .. "]")
 						end
-						if (LastSeenItems[itemID]["sourceInfo"].name ~= source) then
-							LastSeenItems[itemID]["sourceInfo"].name = source
+						-- The item shouldn't be filtered out, so let's add it to the Items table.
+						if (LastSeenItems[itemID]) then
+							-- This isn't a NEW item. Confirm if any information requires an update.
+							if (LastSeenItems[itemID]["sourceInfo"].lootDate ~= lootDate) then
+								LastSeenItems[itemID]["sourceInfo"].lootDate = lootDate
+							end
+							if (LastSeenItems[itemID]["sourceInfo"].patch ~= patch) then
+								LastSeenItems[itemID]["sourceInfo"].patch = patch
+							end
+							if (LastSeenItems[itemID]["sourceInfo"].map ~= addonTable.uiMapName) then
+								LastSeenItems[itemID]["sourceInfo"].map = addonTable.uiMapName
+							end
+							if (LastSeenItems[itemID]["sourceInfo"].name ~= source) then
+								LastSeenItems[itemID]["sourceInfo"].name = source
+							end
+							if (LastSeenItems[itemID].class ~= addonTable.playerClass) then
+								LastSeenItems[itemID].class = addonTable.playerClass
+							end
+							if (LastSeenItems[itemID].level ~= addonTable.playerLevel) then
+								LastSeenItems[itemID].level = addonTable.playerLevel
+							end
+						else
+							-- This is a new item.
+							LastSeenItems[itemID] = {
+								itemName = itemName,
+								itemLink = itemLink,
+								itemQuality = itemQuality,
+								itemLevel = itemLevel,
+								itemMinLevel = itemMinLevel,
+								itemType = itemType,
+								itemSubType = itemSubType,
+								itemStackCount = itemStackCount,
+								itemEquipLoc = itemEquipLoc,
+								itemTexture = itemTexture,
+								itemSellPrice = itemSellPrice,
+								itemClassID = itemClassID,
+								itemSubclassID = itemSubclassID,
+								itemBindType = itemBindType,
+								itemExpacID = itemExpacID,
+								itemSetID = itemSetID,
+								itemIsCraftingReagent = itemIsCraftingReagent,
+								playerInfo = {
+									race = addonTable.playerRace,
+									class = addonTable.playerClass,
+									level = addonTable.playerLevel,
+								},
+								sourceInfo = {
+									name = source,
+									map = map,
+									lootDate = lootDate,
+									patch = patch,
+								},
+							}
+							print(L["Colored Addon Name"] .. L["Green Plus"] .. "|T" .. itemTexture .. ":0|t" .. itemLink)
 						end
-						if (LastSeenItems[itemID].class ~= addonTable.playerClass) then
-							LastSeenItems[itemID].class = addonTable.playerClass
-						end
-						if (LastSeenItems[itemID].level ~= addonTable.playerLevel) then
-							LastSeenItems[itemID].level = addonTable.playerLevel
-						end
-					else
-						-- This is a new item.
-						LastSeenItems[itemID] = {
-							itemName = itemName,
-							itemLink = itemLink,
-							itemQuality = itemQuality,
-							itemLevel = itemLevel,
-							itemMinLevel = itemMinLevel,
-							itemType = itemType,
-							itemSubType = itemSubType,
-							itemStackCount = itemStackCount,
-							itemEquipLoc = itemEquipLoc,
-							itemTexture = itemTexture,
-							itemSellPrice = itemSellPrice,
-							itemClassID = itemClassID,
-							itemSubclassID = itemSubclassID,
-							itemBindType = itemBindType,
-							itemExpacID = itemExpacID,
-							itemSetID = itemSetID,
-							itemIsCraftingReagent = itemIsCraftingReagent,
-							playerInfo = {
-								race = addonTable.playerRace,
-								class = addonTable.playerClass,
-								level = addonTable.playerLevel,
-							},
-							sourceInfo = {
-								name = source,
-								map = map,
-								lootDate = lootDate,
-								patch = patch,
-							},
-						}
-						print(L["Colored Addon Name"] .. L["Green Plus"] .. "|T" .. itemTexture .. ":0|t" .. itemLink)
 					end
 				end
 			end
