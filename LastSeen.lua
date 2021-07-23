77010ba7574ddfb570a404db24609f66c37a8509
Saaappi <alex.metz1024@gmail.com>
2021-07-23 01:47:35 -0500

    Removed the combat check for map updates.

--- LastSeen.lua
+++ LastSeen.lua
@@ -10,18 +10,6 @@ e:RegisterEvent("PLAYER_LEVEL_CHANGED")
 e:RegisterEvent("PLAYER_LOGIN")
 e:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 
-local function IsUnitInCombat(unit)
-	C_Timer.After(0, function()
-		C_Timer.After(1, function()
-			if (UnitAffectingCombat(unit)) then
-				IsUnitInCombat(unit)
-			else
-				return false
-			end
-		end)
-	end)
-end
-
 e:SetScript("OnEvent", function(self, event, ...)
 	if (event == "ADDON_LOADED") then
 		local addon = ...
@@ -55,11 +43,9 @@ e:SetScript("OnEvent", function(self, event, ...)
 	end
 	
 	if (event == "ZONE_CHANGED_NEW_AREA") then
-		if (IsUnitInCombat("player") == false) then
-			uiMapID = C_Map.GetBestMapForUnit("player")
-			uiMapName = C_Map.GetMapInfo(uiMapID).name
-			addonTable.uiMapName = uiMapName
-		end
+		uiMapID = C_Map.GetBestMapForUnit("player")
+		uiMapName = C_Map.GetMapInfo(uiMapID).name
+		addonTable.uiMapName = uiMapName
 	end
 end)
 
